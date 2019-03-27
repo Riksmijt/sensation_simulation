e3f4a636e3cfaf7ac4083409b4fb7abb9a60f450
Rik Smit <rik.smit2912@gmail.com>
2019-03-27 11:50:15 +0100

    voegt de functie input toe en voegt de class Save toe

--- robject.h
+++ robject.h
@@ -7,41 +7,21 @@
 #include <iostream>
 #include <glfw3.h>
 #include <glm/glm.hpp>
-enum KeyCode
-{
-	Right = 262,
-	Left = 263,
-	Down = 264,
-	Up = 265,
-};
+#include <sensation_simulation/Input.h>
+
 class Robject {
 public:
 
 	Robject();
 	 void add(Sprite* f);
-	 //bool getKey(char key) 
-	// {
-	//	 return getKey((KeyCode)key); 
-	// }
+	
 	 //void get(Sprite* f);
 	 //void addSprite(const std::string& filename, int pos_x, int pos_y);
 	~Robject();
 	Sprite* s;
-
+	Input* input;
 	glm::vec3 position;
-/*private:
-	GLFWwindow* _window;
-	void _handleKey(unsigned int key);
-	bool _keys[GLFW_KEY_LAST]; ///< @brief internal array of pressed keys
-	bool _keysUp[GLFW_KEY_LAST]; ///< @brief internal array of released keys
-	bool _keysDown[GLFW_KEY_LAST]; ///< @brief internal array of keys pressed the first time
-
-	bool _mouse[GLFW_MOUSE_BUTTON_LAST]; ///< @brief internal array of pressed mouse buttons
-	bool _mouseUp[GLFW_MOUSE_BUTTON_LAST]; ///< @brief internal array of released mouse buttons
-	bool _mouseDown[GLFW_MOUSE_BUTTON_LAST]; ///< @brief internal array of mouse buttons pressed the first time
-
-	int _windowWidth; ///< @brief Width of the window
-	int _windowHeight;
-	*/
+private:
+	//Input* _input;
 };
 #endif
